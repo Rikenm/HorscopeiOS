29ea75d64c79f63c104c50b51b6ebb82a1237946
Riken Maharjan <rikenm@bu.edu>
2018-05-15 15:14:27 -0400

    Initial Commit

---
+++ Horscope/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Horscope
+//
+//  Created by Riken Maharjan on 5/15/18.
+//  Copyright © 2018 Riken Maharjan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
