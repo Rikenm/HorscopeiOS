b2f4744ba7ce33d4755786d97a3a192d13257520
Riken Maharjan <rikenm@bu.edu>
2018-05-20 11:24:09 -0400

    finish first view

--- Horscope/HomeView/TopView.swift
+++ Horscope/HomeView/TopView.swift
@@ -16,17 +16,23 @@ class TopView: UIView {
     let horoLabel:UILabel = {
         let lv = UILabel()
         lv.text = "Libra"
-        lv.backgroundColor = .green
+        //lv.backgroundColor = .green
+        lv.setContentHuggingPriority(.defaultHigh, for: .horizontal)
+        lv.font = UIFont(name: "AmericanTypewriter", size: 36)
+        lv.textColor =  UIColor(displayP3Red: 83/255, green: 99/255, blue: 127/255, alpha: 1)
+        
         lv.translatesAutoresizingMaskIntoConstraints = false
         return lv
     }()
     
-    let horoPic:UIImageView = {
+    var horoPic:UIImageView = {
         
         let iv = UIImageView()
-        iv.backgroundColor = .red
-        iv.image = UIImage(named: "Libra")
-        iv.sizeToFit()
+    
+        
+        iv.setContentHuggingPriority(.defaultLow, for: .horizontal)
+        
+        iv.sizeThatFits(CGSize(width: 65, height: 65))
         iv.translatesAutoresizingMaskIntoConstraints = false
         return iv
     
@@ -36,13 +42,15 @@ class TopView: UIView {
        
         let label = UILabel()
         label.text = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s."
-        label.backgroundColor = .purple
+        //label.backgroundColor = .purple
         
         label.numberOfLines = 0
         label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont(name: "AmericanTypewriter", size: 16)
+        label.textColor = UIColor(displayP3Red: 83/255, green: 99/255, blue: 127/255, alpha: 1)
         
         // multiple line
-        label.setContentHuggingPriority(.defaultHigh, for: .vertical)
+        //label.setContentHuggingPriority(.defaultHigh, for: .vertical)
         
         return label
         
@@ -52,6 +60,9 @@ class TopView: UIView {
        
         let lb = UILabel()
         lb.text = "Today's Prediction"
+        lb.textColor = UIColor(displayP3Red: 131/255, green: 172/255, blue: 255/255, alpha: 1)
+        lb.font = UIFont(name: "HelveticaNeue", size: 14)
+        
         lb.translatesAutoresizingMaskIntoConstraints = false
         return lb
         
@@ -62,6 +73,9 @@ class TopView: UIView {
         super.init(frame: frame)
         
         backgroundColor = UIColor(displayP3Red: 192/255, green: 213/255, blue: 255/255, alpha: 1)
+        
+        
+        
         
         setUpView()
     }
@@ -82,27 +96,61 @@ class TopView: UIView {
         
         
         // label
-        horoLabel.centerXAnchor.constraint(equalTo: centerXAnchor, constant: 0).isActive = true
+        //horoLabel.centerXAnchor.constraint(equalTo: centerXAnchor, constant: 0).isActive = true
         horoLabel.topAnchor.constraint(equalTo: topAnchor, constant: 20).isActive = true
+        horoLabel.leadingAnchor.constraint(equalTo: horoPic.trailingAnchor, constant: 16).isActive = true
         
         
         // paragraph
+        horoPara.topAnchor.constraint(equalTo: horoPic.bottomAnchor, constant: 12).isActive = true
         horoPara.centerXAnchor.constraint(equalTo: centerXAnchor, constant: 0).isActive = true
-        horoPara.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -16).isActive = true
-        horoPara.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 8).isActive = true
-        horoPara.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -8).isActive = true
+       // horoPara.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -16).isActive = true
+        horoPara.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 20).isActive = true
+        horoPara.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -20).isActive = true
     
         // constant prediction text
-        constLabel.topAnchor.constraint(equalTo: horoLabel.bottomAnchor, constant: 8).isActive = true
+        constLabel.topAnchor.constraint(equalTo: horoLabel.bottomAnchor, constant: -1).isActive = true
         constLabel.leadingAnchor.constraint(equalTo: horoLabel.leadingAnchor,constant: -4).isActive = true
         
         
+        horoPic.image = resizeImage(image:#imageLiteral(resourceName: "Libra"), targetSize: CGSize(width: 65.0, height: 65.0))
+        
         // image
-        horoPic.trailingAnchor.constraint(equalTo: horoLabel.leadingAnchor, constant: -8).isActive = true
+        //horoPic.trailingAnchor.constraint(equalTo: horoLabel.leadingAnchor, constant: -8).isActive = true
+        horoPic.centerXAnchor.constraint(equalTo: centerXAnchor, constant : -100).isActive = true
+        horoPic.topAnchor.constraint(equalTo: topAnchor, constant: 15).isActive = true
         
         
         
     }
+    
+    func resizeImage(image: UIImage, targetSize: CGSize) -> UIImage {
+        let size = image.size
+        
+        let widthRatio  = targetSize.width  / size.width
+        let heightRatio = targetSize.height / size.height
+        
+        // Figure out what our orientation is, and use that to form the rectangle
+        var newSize: CGSize
+        if(widthRatio > heightRatio) {
+            newSize = CGSize(width: size.width * heightRatio, height: size.height * heightRatio)
+        } else {
+            newSize = CGSize(width: size.width * widthRatio,  height: size.height * widthRatio)
+        }
+        
+        // This is the rect that we've calculated out and this is what is actually used below
+        let rect = CGRect(x: 0, y: 0, width: newSize.width, height: newSize.height)
+        
+        // Actually do the resizing to the rect using the ImageContext stuff
+        UIGraphicsBeginImageContextWithOptions(newSize, false, 1.0)
+        image.draw(in: rect)
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return newImage!
+    }
+    
+    
     
     
     
